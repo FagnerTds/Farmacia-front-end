852aecaa149f7f98c496b26999d69c61d4407ff5
Fagner Tranquilino <fagnertds@gmail.com>
2023-10-17 12:48:04 -0300

    home, navbar e footer corrigidos

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,40 @@
-import React from 'react';
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
 import './App.css';
-import Home from './paginas/home/Home';
+import 'react-toastify/dist/ReactToastify.css';
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
+import { ToastContainer } from 'react-toastify';
+
 import Navbar from './components/navBar/NavBar';
+import Home from './paginas/home/Home';
 import Footer from './components/footer/Footer';
 
+
 function App() {
+  
   return (
     <>
-    <Navbar/>
-    <Home/>
-    <Footer/>
+      <AuthProvider>
+      <ToastContainer />
+        <BrowserRouter>
+        <Navbar />
+          <div className='min-h-[80vh]'>
+            <Routes>
+              
+              
+              
+              <Route path="/home" element={<Home />} />
+            
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
     </>
-);
+  );
 }
+
 export default App;
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,21 +1,13 @@
+import React from 'react'
 import { FacebookLogo, InstagramLogo, LinkedinLogo } from '@phosphor-icons/react'
-import React, { useContext } from 'react'
-import { AuthContext } from '../../contexts/AuthContext'
 
 function Footer() {
-
-  const { usuario, handleLogout } = useContext(AuthContext)
-
-  let footerComponent
-
-  let data = new Date().getFullYear()
-
-  if(usuario.token !== '') {
-    footerComponent = (
-      <>
+ 
+  return (
+    <>
         <div className="flex justify-center bg-indigo-900 text-white">
           <div className="container flex flex-col items-center py-4">
-            <p className='text-xl font-bold'>Blog pessoal Generation | Copyright: {data}</p>
+            <p className='text-xl font-bold'>Farmácia Generation | Copyright: 2023 </p>
             <p className='text-lg'>Acesse nossas redes sociais</p>
             <div className='flex gap-2'>
               <LinkedinLogo size={48} weight='bold' />
@@ -25,13 +17,6 @@ function Footer() {
           </div>
         </div>
       </>
-    )
-  }
-
-  return (
-    <>
-      {footerComponent}
-    </>
   )
 }
 
